0f3c1bec8a9e88ab25557f451493b4582e5d7bfe
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-04-13 11:28:15 +0100

    ready to write tests

---
+++ pagescale.go
@@ -0,0 +1,67 @@
+package pagescale
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/timdrysdale/geo"
+	"github.com/unidoc/unipdf/v3/creator"
+)
+
+func PrintImageOnDynamicPage(imgPath string, pageDim geo.Dim, pdfPath string) error {
+
+	c := creator.New()
+
+	img, err := c.NewImageFromFile(imgPath)
+
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error opening image file: %s", err))
+	}
+
+	img.ScaleToHeight(pageDim.H)
+	pageDim.W = img.Width()
+
+	c.SetPageMargins(0, 0, 0, 0) // we're not printing, so margin needed
+	c.SetPageSize(creator.PageSize{pageDim.W, pageDim.H})
+	c.NewPage()
+
+	c.Draw(img)
+
+	c.WriteToFile(pdfPath)
+
+	return nil
+}
+
+func PrintImageOnStaticPage(imgPath string, pageDim geo.Dim, pdfPath string) error {
+
+	c := creator.New()
+	c.SetPageMargins(0, 0, 0, 0) // we're not printing, so margin needed
+	c.SetPageSize(creator.PageSize{pageDim.W, pageDim.H})
+	c.NewPage()
+
+	img, err := c.NewImageFromFile(imgPath)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error opening image file: %s", err))
+	}
+
+	// estimate image width if we scale to full height
+	// note to future self - don't use the library to handle
+	// an actual scaling operation as a means to do the calc
+	// because if you have to rescale, there may be unintended
+	// effects due to changes under the hood of the library
+	// (it'd be ok with current implementation, but ...)
+	imgScaledWidth := img.Width() * pageDim.H / img.Height()
+
+	if imgScaledWidth > pageDim.W {
+		// oops, we're too big, so scale using width instead
+		img.ScaleToWidth(pageDim.W)
+	} else {
+		img.ScaleToHeight(pageDim.H)
+	}
+
+	c.Draw(img)
+
+	c.WriteToFile(pdfPath)
+
+	return nil
+}
---
+++ pagescale_test.go
@@ -0,0 +1,10 @@
+package pagescale
+
+import "testing"
+
+const (
+	a4p = "./test/a4p/jpg"
+)
+
+func TestPrintImageOnStaticPage(t *testing.T) {
+}
